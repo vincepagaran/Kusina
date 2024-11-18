b13efc4343a744bf93a6708bc2958a6301d07d92
Acer@vincepagaran <kaitopagaran@gmail.com>
2024-11-18 23:24:32 +0800

    feat: added reg Func

---
+++ src/components/common/AlertNotification.vue
@@ -0,0 +1,25 @@
+<!-- <script setup>
+const props = defineProps(['formSuccessMessage, formErrorMessage'])
+</script>
+
+<template>
+    <v-alert v-if="props.formSuccessMessage"
+  :text="props.formSuccessMessage"
+  title="success!"
+  type="success"
+  variant="tonal"
+  density="compact"
+  border="start"
+  closable
+  ></v-alert>
+
+  <v-alert v-if="props.formErrorMessage"
+  :text="props.formErrorMessage"
+  title="Error!"
+  type="Error"
+  variant="tonal"
+  density="compact"
+  border="start"
+  closable
+  ></v-alert>
+</template> -->
