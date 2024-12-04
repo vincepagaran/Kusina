5ed5be2db2c5f4e68514c6eb9dfb83e6e9de9cc6
Jacky-Barneso <jakivllarn07@gmai,com>
2024-12-04 21:37:09 +0800

    fix: resolve merge

--- src/views/system/MenuDishes.vue
+++ src/views/system/MenuDishes.vue
@@ -62,9 +62,9 @@
                   <h3><strong style="color: #4caf50; font-weight: bold">Instructions:</strong></h3>
                   <div v-html="recipe.instructions"></div>
                 </v-card-text>
-                
+
                 <div v-if="currentStep < totalSteps - 1">
-                
+
                   <v-btn @click="previousStep" :disabled="currentStep === 0">Previous</v-btn>
                   <v-btn @click="skipStep">Next</v-btn>
                 </div>
